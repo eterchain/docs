33d04fd34c80138bf4df192b68064112bce3b8de
nrif_ <naftaryandikaisyacafahmi@gmail.com>
2025-02-03 18:28:17 +0700

    php

--- php/get_data.php
+++ php/get_data.php
@@ -6,6 +6,8 @@ include 'connection.php';
 
 if (isset($_GET['get_data'])) {
     get_data($conn);
+} else if (isset($_GET['get_summary'])) {
+    get_summary($conn);
 }
 
 
@@ -20,6 +22,30 @@ function get_data($conn)
                 SUM(speed) AS speed 
             FROM eterchain_db.`user` 
             GROUP BY wallet";
+
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    if (mysqli_num_rows($result) > 0) {
+        $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+        echo json_encode($data);
+    } else {
+        echo json_encode(['msg' => 'No Data!', 'status' => false]);
+    }
+}
+
+function get_summary($conn)
+{
+    $query = "SELECT 
+                COUNT(wallet) AS totWallet, 
+                SUM(point) AS totPoint, 
+                SUM(worker) AS totWorker, 
+                SUM(speed) AS totSpeed
+            FROM
+            eterchain_db.`user`";
 
     $stmt = $conn->prepare($query);
     $stmt->execute();
